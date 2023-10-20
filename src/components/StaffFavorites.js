b50635aa48f4caa991c0ca1842381d357b8a9dcb
Donovan Rimer <donnyjrimer@gmail.com>
2023-10-19 20:19:47 -0700

    making progress, needs more tweaking

--- src/components/StaffFavorites.js
+++ src/components/StaffFavorites.js
@@ -22,17 +22,17 @@ const GreenBox = styled("div")`
 `;
 
 const SFText = styled("div")`
-width: 449px
-height: 39px
-top: 3938px
-left: 292px
-font-family: Gotham;
+width: 449px;
+height: 39px;
+color: white;
+font-family: Arial;
 font-size: 32px;
 font-weight: 700;
 line-height: 31px;
 letter-spacing: 0em;
 text-align: left;
-background: #FFFFFF;
+margin-left: 20px;
+margin-top: 12px;
 `
 
 const WhiteBox = styled("div")`
@@ -43,6 +43,42 @@ const WhiteBox = styled("div")`
 
 `;
 
+const BrownBox = styled("div")`
+width: 720px;
+height: 387px;
+background-color: #7C2F18;
+border-radius: 10px;
+margin-left: auto;
+margin-right: auto;
+margin-top: 10px;   
+`
+
+const LittleBox = styled("div")`
+width: 740px;
+height: 407px;
+margin-left: auto;
+margin-right: auto;
+margin-top: 80px;
+border: 2px solid black;
+z-index: 1;
+`
+
+const Caption = styled("div")`
+width: 719px;
+height: 66px;
+margin-top: 20px;
+margin-left: 91px;
+font-family: Times;
+font-size: 32px;
+font-weight: 400;
+line-height: 44px;
+letter-spacing: 0.07em;
+text-align: left;
+color: black;
+z-index: 1;
+position: relative;
+`
+
 const StaffFavorites = () => {
   return (
     <Container>
@@ -52,6 +88,10 @@ const StaffFavorites = () => {
             STAFF FAVORITES
             </SFText>
          </GreenBox>
+            <LittleBox>
+            <BrownBox></BrownBox>
+            </LittleBox>
+            <Caption> text goes here</Caption>
          </WhiteBox>
     </Container>
   )
